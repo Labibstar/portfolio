c29bc320d6a996558c94e8ee8f5423717af543b1
labibstar <labibstar@gmail.com>
2023-10-18 00:04:16 +0600

    Centered text and chose bg

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
-import bg from '../public/images/background1.jpg'
+import bg6 from '../public/images/bg6.jpg'
 import bg2 from '../public/images/background2.jpg'
+import bg3 from '../public/images/bg3.jpg'
+import bg4 from '../public/images/bg4.jpg'
 
 export default function Home() {
   return (
@@ -8,26 +10,47 @@ export default function Home() {
       <title>Portfolio</title>
 
   
-    <div className="h-screen grid grid-cols-2 gap-4 mt-2 place-items-center">
-      <div className="text-center place-items-center place-content-center" style={
-        {backgroundImage: `url(${bg2.src})`,
-         width: "100%",
-         height: "100%",
-         zIndex: -2 
-        } }>
-      <h1 style={{position: "relative", zIndex: 2}}>Hi I'm Labib</h1>
+    <div className="h-screen grid grid-cols-2  bg-contain place-content-center">
+
+      <div className="grid text-center bg-contain bg-center place-content-center" >
+        <h1 style={{position: "relative", zIndex: 2}}>Hi</h1>
       </div>
-      <div className="text-center place-content-center bg-local" style={
-        {backgroundImage: `url(${bg.src})`,
-         width: "100%",
-         height: "100%",
-         zIndex: -2 
+      <div className=" grid text-center place-content-center bg-fixed bg-cover bg-[top_left_30rem] bg-[translate_x_30]" style={
+        {backgroundImage: `url(${bg6.src})`,
+         
+         height: "100vh",
+         
         }
     }>
-      <h1 className='place-content-center'>This is my portfolio</h1>
+        <h1 style={{position: "relative", zIndex: 2, color: 'black'}}>I'm Labib</h1>
       </div>
+
+      
     </div>
     
+    
+    <div className="h-screen grid grid-cols-2 bg-cover place-content-center">
+    
+      <div className="grid text-center place-items-center place-content-center bg-fixed" style={
+      {backgroundImage: `url(${bg3.src})`,
+      
+      height: "100vh",
+      
+    } }
+    >
+      <h1 style={{position: "relative", zIndex: 2}}>placeholder text</h1>
+
+
+
+      </div>
+    <div className=" grid text-center place-items-center align-center content-center items-center">
+      <h1 style={{position: "relative", zIndex: 2}}>placeholder text</h1>
+
+
+      
+      </div>
+
+          </div>    
    
   
 
