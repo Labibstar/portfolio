50c4246da6607db92094a21118ad4addde02ce15
labibstar <labibstar@gmail.com>
2023-11-03 22:23:32 +0600

    Revert "Revert "Back to basics""
    This reverts commit 347d0eb19081a480382f00fc0e8311add127f033.

--- app/Hero.js
+++ app/Hero.js
@@ -7,15 +7,15 @@ function Hero({
 
 }) {
   return (
-  <div id="sec1" className="hero h-screen grid grid-cols-2 snap-end bg-contain place-content-center">
+  <div id="sec1" className="hero h-screen grid grid-cols-2 bg-contain place-content-center">
 
-    <div className="grid text-center bg-contain bg-center place-content-center">
+    <div className="grid text-center place-content-center">
       <h1 style={{
         position: "relative",
         zIndex: 2
       }}>placeholdertext0</h1>
     </div>
-    <div className=" grid text-center place-content-center bg-fixed bg-cover bg-no-repeat bg-[left_top_-40rem] bg-[scale-25]" style={{
+    <div className="bgo grid text-center place-content-center bg-cover bg-no-repeat" style={{
         backgroundImage: `url(${bgsrc.src})`,
         height: "100vh"
     }}>
