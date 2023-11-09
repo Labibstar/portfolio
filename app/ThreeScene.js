940ef85defa2dabe9b1859d58bd70a8f721c6b4e
labibstar <labibstar@gmail.com>
2023-11-10 04:11:33 +0600

    Deployment v0.1

--- app/ThreeScene.js
+++ app/ThreeScene.js
@@ -86,7 +86,7 @@ const ThreeScene = () => {
             <svg className="w-6 h-6 ml-[30rem] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
 </svg> </p>
-<p>
+<p style={{color: "white"}}>
  Click and Drag to explore the sunset
  </p>
  <p>
