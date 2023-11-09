37942af2990754288e63ee61b6735a2a82229d21
labibstar <labibstar@gmail.com>
2023-11-10 03:42:06 +0600

    Deployment v0.1

--- app/ThreeScene.js
+++ app/ThreeScene.js
@@ -83,14 +83,14 @@ const ThreeScene = () => {
             <div className='info3 grid grid-cols-3'>
             <p>
 
-            <svg className="w-6 h-6 ml-96 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+            <svg className="w-6 h-6 ml-[27rem] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
 </svg> </p>
 <p>
  Click and Drag to explore the sunset
  </p>
- <p className='align-items-end'>
-<svg className="w-6 h-6 text-gray-800 mr-96 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+ <p>
+<svg className="w-6 h-6 mr-[27rem] text-gray-800  dark:text-white" style={{display:"flex"}} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
 </svg>
             </p>
