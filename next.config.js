ec6c5681e76e76498f004a576c47e1894c9019b3
labibstar <labibstar@gmail.com>
2023-11-10 02:08:36 +0600

    Deployment v0.1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true }
+    images: { 
+        loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+        unoptimized: true }
 }
 
 module.exports = nextConfig
