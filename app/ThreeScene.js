ba04603fc6ecbbe3e45194bbb5ecf6015ba8d301
labibstar <labibstar@gmail.com>
2023-11-10 03:57:46 +0600

    Deployment v0.1

--- app/ThreeScene.js
+++ app/ThreeScene.js
@@ -83,7 +83,7 @@ const ThreeScene = () => {
             <div className='info3 grid grid-cols-3'>
             <p>
 
-            <svg className="w-6 h-6 ml-[27rem] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+            <svg className="w-6 h-6 ml-[31rem] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
 </svg> </p>
 <p>
