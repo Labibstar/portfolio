0241a72052900725e16727d9ea0c90639f29f87f
labibstar <labibstar@gmail.com>
2023-11-11 04:12:46 +0600

    Deployment v0.3

--- app/ThreeScene.js
+++ app/ThreeScene.js
@@ -46,7 +46,7 @@ const ThreeScene = () => {
           scene.add(spotlight);
           
           const rectlight = new THREE.RectAreaLight(0xffffff, 1.5, 200, 200);
-          rectlight.position.set(0,2, -4);
+          rectlight.position.set(0,2, -2.1);
           scene.add(rectlight);
    
 
@@ -87,20 +87,11 @@ const renderLoading =(
 );
 
 const renderLoaded =(
-  <div className='info3 grid grid-cols-3'>
-            <p>
-
-            <svg className="w-6 h-6 ml-[30rem] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
-</svg> </p>
+  <div className='info3 grid'>
+            
 <p style={{color: "white"}}>
  Click and Drag to explore the sunset
  </p>
- <p>
-<svg className="w-6 h-6 mr-[27rem] text-gray-800  dark:text-white" style={{display:"flex"}} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
-</svg>
-            </p>
             </div>
             
 )
